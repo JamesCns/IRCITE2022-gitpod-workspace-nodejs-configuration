476b87e44415ef242c8114e706628c9e78e23db5
James Russel <cinensejames@gmail.com>
2022-04-07 15:19:13 +0000

    few changes

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// sample GET method 
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+})
+
+// listen for requests in port 4000
+app.listen(process.env.port || port, function(){
+    console.log('Now listening for requests...');
+});
+
